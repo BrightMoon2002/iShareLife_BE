ca0b9c44b042aab1dd9cee2c5acf18cfb0d0bd98
nhimdom200292 <kiemda02@gmail.com>
2022-01-12 01:57:15 +0700

    add fix change avatar

--- src/main/java/com/example/isharelife/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/example/isharelife/security/jwt/JwtTokenFilter.java
@@ -1,5 +1,6 @@
 package com.example.isharelife.security.jwt;
 
+import com.example.isharelife.security.userprincipal.AccountDetailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,34 +22,28 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
     @Autowired
     private JwtProvider jwtProvider;
-
     @Autowired
     private UserDetailsService userDetailsService;
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             String token = getJwt(request);
-            if (token != null && jwtProvider.validateToken(token)) {
-                String username = jwtProvider.getUsernameFromToken(token);
+            if(token !=null &&jwtProvider.validateToken(token)){
+                String username = jwtProvider.getUserNameFromToken(token);
                 UserDetails userDetails = userDetailsService.loadUserByUsername(username);
                 UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities()
-                );
-                //build user ở trên web
+                        userDetails, null, userDetails.getAuthorities());
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
-        } catch (Exception e) {
-            logger.error("Cant set user authentication -> Message: {}", e);
+        } catch (Exception e){
+            logger.error("Can't set user authentication -> Message: {}",e);
         }
-        //chưua hiểu là để làm gì lắm cần phải hỏi lại giáo viên
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request,response);
     }
-
-    private String getJwt(HttpServletRequest request) {
+    private String getJwt(HttpServletRequest request){
         String authHeader = request.getHeader("Authorization");
-        if (authHeader != null && authHeader.startsWith("Bearer")) {
+        if(authHeader !=null && authHeader.startsWith("Bearer")){
             return authHeader.replace("Bearer", "");
         }
         return null;
