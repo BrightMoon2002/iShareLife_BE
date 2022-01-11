510fd368b4ac0568675220b6327abec5fbc34528
hieuhvh1994 <hieudaohn94@gmail.com>
2022-01-11 15:17:52 +0700

    fixed bugs repository

--- src/main/java/com/example/isharelife/repository/chat_room/IStatusChatRoomMessageTypeRepository.java
+++ src/main/java/com/example/isharelife/repository/chat_room/IStatusChatRoomMessageTypeRepository.java
@@ -2,6 +2,8 @@ package com.example.isharelife.repository.chat_room;
 
 import com.example.isharelife.model.chat_room.StatusChatRoomMessageType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IStatusChatRoomMessageTypeRepository extends JpaRepository<StatusChatRoomMessageType, Long> {
 }
