69300aec1b58d31d906bae2b48e2d6a3eed8bf6e
batuanr <tuanpesi@gmail.com>
2022-01-18 09:26:11 +0700

    update notification

--- src/main/java/com/example/isharelife/model/notification/Notification.java
+++ src/main/java/com/example/isharelife/model/notification/Notification.java
@@ -1,11 +1,10 @@
 package com.example.isharelife.model.notification;
 
+import com.example.isharelife.model.account.Account;
+import com.example.isharelife.model.post.Posting;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -13,5 +12,12 @@ public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String content;
+//    private String action;
+    @ManyToOne(targetEntity = Account.class)
+    private Account account;
+    @OneToOne
+    @JoinColumn(name = "posting_id")
+    private Posting posting;
+    private Boolean status;
 }
