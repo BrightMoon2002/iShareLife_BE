4a1bf7ad1f27616953793f126c33485b456b06a4
batuanr <tuanpesi@gmail.com>
2022-01-18 17:28:52 +0700

    update noti

--- src/main/java/com/example/isharelife/controller/notification/NotificationController.java
+++ src/main/java/com/example/isharelife/controller/notification/NotificationController.java
@@ -1,8 +1,38 @@
 package com.example.isharelife.controller.notification;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.isharelife.model.account.Account;
+import com.example.isharelife.model.notification.Notification;
+import com.example.isharelife.service.IAccountService;
+import com.example.isharelife.service.notification.INotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.method.P;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
+@RequestMapping("/notification")
 public class NotificationController {
+    @Autowired
+    INotificationService notificationService;
 
+    @Autowired
+    IAccountService accountService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getAll(@PathVariable Long id){
+        Account account = accountService.findAccountById(id).get();
+        return new ResponseEntity<>(notificationService.findAllByAccountOrderByIdDesc(account), HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Notification notification){
+        notificationService.save(notification);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        notificationService.remove(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/isharelife/model/notification/Notification.java
+++ src/main/java/com/example/isharelife/model/notification/Notification.java
@@ -2,22 +2,30 @@ package com.example.isharelife.model.notification;
 
 import com.example.isharelife.model.account.Account;
 import com.example.isharelife.model.post.Posting;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Notification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
 //    private String action;
+    @OneToOne
+    @JoinColumn(name = "sender_id")
+    private Account sender;
     @ManyToOne(targetEntity = Account.class)
     private Account account;
     @OneToOne
     @JoinColumn(name = "posting_id")
     private Posting posting;
     private Boolean status;
+
 }
